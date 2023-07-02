6172482b6305a36b59e362706f888c7dbd698afd
nimbusmustafa <mustafa.mitmpl2022@learner.manipal.edu>
2023-07-03 01:22:00 +0530

    chatroom

---
+++ chatroom/readme.md
@@ -0,0 +1,8 @@
+#Description 
+Using ROS make a chatroom for users where they can view and send messages that all users can see. Ensure that the chat room has at least 3 users/nodes that connect to the same.
+ros topic published is chatroom
+ros messgages in string type and services are subscriber and publisher
+#rqt graph
+![Screenshot from 2023-02-01 19-07-59](https://user-images.githubusercontent.com/117943931/216240061-859acd70-69bd-41b5-8b47-2d452fae1af2.png)
+#youtube link 
+https://youtu.be/8aUIy2Tuo40
---
+++ chatroom/src/node1.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <chatroom/msge.h>
+
+using namespace std;
+
+void Callback(const chatroom::msge::ConstPtr& msg) {
+  ROS_INFO("%s: %s", msg->username.c_str(), msg->chat.c_str());
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "node1");
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub = nh.subscribe("chatroom", 1000, Callback);
+  ros::Publisher pub = nh.advertise<chatroom::msge>("chatroom", 1000);
+
+  chatroom::msge msg;
+
+  string user;
+  cout<<"Enter Name: ";
+  getline(cin,user);
+  msg.username = user;
+
+  ros::AsyncSpinner spinner(3);
+  spinner.start();
+
+  while (ros::ok()) {
+    string input;
+    getline(cin, input);
+    msg.chat = input;
+    pub.publish(msg);
+  }
+
+  return 0;
+}
